55fc8fe4ac40c78162a83c0a1a7c6fdb43dc6398
Tigran <mherannas@gmail.com>
2025-12-26 17:15:51 +0400

    Feature: components-ui

--- src/game/Millionaire.tsx
+++ src/game/Millionaire.tsx
@@ -1,3 +1,33 @@
+import { useState } from "react";
+import { MillionaireDashboard } from "./components/MillionaireDashboard";
+import { questions } from "./data/questions";
+import { OptionEnum } from "./enums/Options.enum";
+
 export const Millionaire = () => {
-  return <>Millionaire</>;
+  const [level, setLevel] = useState(13);
+
+  const handleOptionSelect = (selectedOption: OptionEnum) => {
+    const currentQuestion = questions[level];
+    const isCorrect = selectedOption === currentQuestion.correctOption;
+
+    if (isCorrect) {
+      if (level === questions.length - 1) {
+        return;
+      }
+
+      setTimeout(() => {
+        setLevel((prev) => prev + 1);
+      }, 1000);
+    } else {
+      window.alert("❌ You lost!");
+      setLevel(0);
+    }
+  };
+
+  return (
+    <MillionaireDashboard
+      {...questions[level]}
+      onOptionSelect={handleOptionSelect}
+    />
+  );
 };
